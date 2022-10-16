a323db8c55a10a8eab42de732001c0cf8928baa3
Linus Falk <linfa084@gmail.com>
2022-10-16 23:15:55 +0200

    remove cors

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ import resolvers from './graphql/resolvers/index.js'
 import cors from 'cors'
 
 const server = new ApolloServer({
-    cors: {
-        origin: cors({origin: ['http://127.0.0.1:5173/', 'https://splitsville.netlify.app', 'https://studio.apollographql.com']}),
-    },
     typeDefs,
     resolvers,
     context: ({req}) => ({req}),
